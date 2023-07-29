b8584658040094b81186f806ccbd91492354d604
atakanhbk <atakanhbk@gmail.com>
2023-07-29 17:26:42 +0300

    Fixed Bug and Add index3.html

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ AddQuestionNumber();
 CheckResults();
 
 var questionNumberInt = parseInt(localStorage.getItem("questionNumber"));
+
+
 function AddAllButtonsEvents() {
-  
+
     answerBtn.forEach(element => {
         element.addEventListener('click', function () {
             ClickedAnswerButton(element);
@@ -19,16 +21,30 @@ function AddAllButtonsEvents() {
     });
 
     nextBtn.addEventListener('click', function () {
-        window.location = 'index2.html';
-        localStorage.setItem("questionNumber",questionNumberInt + 1);
+
+        let indexName = 'index' + (questionNumberInt + 2) + '.html';
+
+        if (questionNumberInt < 2) {
+            window.location = indexName;
+
+            console.log(parseInt(indexName));
+            localStorage.setItem("questionNumber", questionNumberInt + 1);
+        }
+        else {
+            localStorage.clear();
+            window.location = "index.html";
+        }
+
+
+
 
 
     })
 }
 
 function AddQuestionNumber() {
-    if(localStorage.getItem("questionNumber") === null){
-        localStorage.setItem("questionNumber",0);
+    if (localStorage.getItem("questionNumber") === null) {
+        localStorage.setItem("questionNumber", 0);
     }
 }
 
@@ -37,7 +53,7 @@ function CheckResults() {
     if (JSON.parse(localStorage.getItem("window.location")) != null) {
         results = JSON.parse(localStorage.getItem("window.location"));
 
-
+        console.log(results.length);
 
         if (results[0] == 1) {
             resultsCheck[0].style.display = 'block';
@@ -48,16 +64,32 @@ function CheckResults() {
             resultsX[0].style.display = 'block';
             resultsX[0].parentNode.style.backgroundColor = 'black';
         }
+        if (results.length >= 2) {
+            if (results[1] == 1) {
+                resultsCheck[1].style.display = 'block';
+                resultsCheck[1].parentNode.style.backgroundColor = 'green';
+            }
 
-        if (results[1] == 1 && results.length > 1) {
-            resultsCheck[1].style.display = 'block';
-            resultsCheck[1].parentNode.style.backgroundColor = 'green';
+            else {
+                resultsX[1].style.display = 'block';
+                resultsX[1].parentNode.style.backgroundColor = 'black';
+            }
         }
 
-        else if(results[1] == 1 && results.length > 1) {
-            resultsX[1].style.display = 'block';
-            resultsX[1].parentNode.style.backgroundColor = 'black';
+        if (results.length >= 3) {
+            if (results[2] == 1) {
+                resultsCheck[2].style.display = 'block';
+                resultsCheck[2].parentNode.style.backgroundColor = 'green';
+            }
+
+            else {
+                resultsX[2].style.display = 'block';
+                resultsX[2].parentNode.style.backgroundColor = 'black';
+            }
         }
+
+
+
     }
 
 
@@ -70,7 +102,7 @@ function ClickedAnswerButton(e) {
 
     let results;
 
-    
+
     if (localStorage.getItem("window.location") === null) {
         results = [];
     }
@@ -80,13 +112,15 @@ function ClickedAnswerButton(e) {
     }
 
     if (e.value == "correct") {
+        console.log(resultsCheck);
         e.style.backgroundColor = 'green';
-        resultsCheck[questionNumberInt].style.display = 'block';
-        resultsCheck[questionNumberInt].parentNode.style.backgroundColor = 'green';
+        resultsCheck[2].style.display = 'block';
+        resultsCheck[2].parentNode.style.backgroundColor = 'green';
         results.push(1);
 
     }
     else {
+        console.log(resultsX);
         e.style.backgroundColor = 'red';
         resultsX[questionNumberInt].style.display = 'block';
         resultsX[questionNumberInt].parentNode.style.backgroundColor = 'black';
